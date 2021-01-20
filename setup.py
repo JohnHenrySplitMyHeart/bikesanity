8e7ffd4f1b413d0b63d9400b313934912392dd4a
John Henry <john.henry.split.my.heart@protonmail.com>
2021-01-20 00:58:26 +0000

    Updated setup.py for release

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JohnHenrySplitMyHeart/bikesanity",
+    download_url = 'https://github.com/JohnHenrySplitMyHeart/bikesanity/archive/v_100.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
